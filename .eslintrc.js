fc4f858a210afba558ef4d62906ea7f7895a2346
hamjongseok <irang4605@naver.com>
2023-01-30 00:08:56 +0900

    [feat]프리티어 세팅 및 detail css 작업중

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended",
+		'plugin:prettier/recommended', 'prettier'],
+    "plugins": ['prettier'],
     "overrides": [
     ],
     "parserOptions": {
