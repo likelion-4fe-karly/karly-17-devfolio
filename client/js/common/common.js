ca8b265bc5afc4d176c576b5f8a38fc7a1ffa514
SSY1203 <tlstjsdud566@gmail.com>
2023-01-31 19:20:55 +0900

    [Feature] : 카테고리 버튼 클릭 시 서브 메뉴 나오게 구현

--- client/js/common/common.js
+++ client/js/common/common.js
@@ -1,5 +1,5 @@
-window.addEventListener('load', function () {
-  const button = document.querySelector('.nav_category button');
+// window.addEventListener('load', function () {
+//   const button = document.querySelector('.nav_category button');
 
-  console.log(button);
-});
+//   console.log(button);
+// });
--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,21 @@
-import { includeIframeToHTML } from './../lib/include/includeIframeToHTML.js';
-import {} from './common/common.js';
+import { includeIframeToHTML } from '../lib/include/includeIframeToHTML.js';
 
-includeIframeToHTML();
+includeIframeToHTML()
+  .then(() => {
+    const body = document.querySelector('body');
+
+    const bodyHandler = (e) => {
+      const category = e.target.closest('button');
+      const subCategory = document.querySelector('.category_menu');
+      if (category) {
+        subCategory.style.display = 'block';
+        return;
+      }
+      subCategory.style.display = 'none';
+    };
+
+    body.addEventListener('click', bodyHandler);
+  })
+  .catch(() => {
+    console.error('실패');
+  });
