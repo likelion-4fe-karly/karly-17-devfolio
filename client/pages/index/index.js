3265669d5b67cdc33b38ff77cb7bf5a07b55418b
kelly121212 <cbby1212@gmail.com>
2023-02-01 17:36:48 +0900

    [Feature] : 최근 본 상품 마크업 제작

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -3,7 +3,7 @@
 /* global Swiper */
 
 /* 메인 배너 */
-const swiper = new Swiper('.index_visual_swiper', {
+const swiper1 = new Swiper('.index_visual_swiper', {
   autoplay: true,
   pagination: {
     el: '.swiper-pagination',
@@ -31,8 +31,22 @@ const swiper2 = new Swiper('.index_recommend_product_swiper', {
   speed: 1000,
   parallax: true,
   pagination: {},
+  a11y: {
+    prevSlideMessage: '이전 슬라이드',
+    nextSlideMessage: '다음 슬라이드',
+    slideLabelMessage:
+      '총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.',
+  },
   navigation: {
     prevEl: '.index_recommend_product_swiper .swiper-button-prev',
     nextEl: '.index_recommend_product_swiper .swiper-button-next',
   },
+});
+
+/* 이 상품 어때? & 놓치면 후회할 가격 */
+const swiper3 = new Swiper('.recent_product_swiper', {
+  direction: 'vertical',
+  autoplay: false,
+  loop: true,
+  speed: 1000,
 });
