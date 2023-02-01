91860d3da2b7476386da132ce7dad4586cd43b43
Simyumi <gkghgkgh0@naver.com>
2023-02-01 18:23:00 +0900

    product_filter 까지

--- client/pages/product_list/product_list.js
+++ client/pages/product_list/product_list.js
@@ -1,10 +1,10 @@
 // accordion 만들기
-var accordion = document.getElementsByClassName('btn_toggle');
-var i;
+let accordion = document.getElementsByClassName('btn_toggle');
+let i;
 for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
     this.classList.toggle('active');
-    var accordion_item_list = this.nextElementSibling;
+    let accordion_item_list = this.nextElementSibling;
     if (!this.classList.contains('active')) {
       accordion_item_list.style.maxHeight = null;
     } else {
