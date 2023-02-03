17d2194186783b1a77768a9edb4b05e283f71733
SSY1203 <tlstjsdud566@gmail.com>
2023-02-03 16:43:34 +0900

    [Feature] : 회원가입 페이지에 체크 박스 이벤트 추가

--- client/pages/signup/signup.js
+++ client/pages/signup/signup.js
@@ -1,11 +1,39 @@
 // 동의 모두선택 / 해제
 
 const agreeCheckAll = document.querySelector('input[name=all_agree]');
+let agreeCheck = document.querySelectorAll('input[name=agree]');
 
-agreeCheckAll.addEventListener('change', (e) => {
-  let agreeCheck = document.querySelectorAll('input[name=agree]');
+agreeCheckAll.addEventListener('click', (e) => {
+  agreeCheckAll.classList.toggle('active');
 
-  for (let i = 0; i < agreeCheck.length; i++) {
-    agreeCheck[i].checked = e.target.checked;
-  }
+  agreeCheck.forEach((check) => {
+    if (agreeCheckAll.classList.contains('active')) {
+      check.setAttribute('checked', true);
+    } else {
+      check.setAttribute('checked', false);
+    }
+    check.checked = e.target.checked;
+  });
 });
+
+agreeCheck.forEach((node) => {
+  node.addEventListener('click', () => {
+    node.classList.toggle('on');
+    if (node.classList.contains('on')) {
+      node.setAttribute('checked', true);
+    } else {
+      node.setAttribute('checked', false);
+    }
+  });
+});
+
+if (
+  agreeCheck[0].checked &&
+  agreeCheck[1].checked &&
+  agreeCheck[2].checked &&
+  agreeCheck[3].checked
+) {
+  agreeCheckAll.checked = true;
+} else {
+  agreeCheckAll.checked = false;
+}
