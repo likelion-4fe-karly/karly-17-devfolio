320f056cf9ea96ccf2e5f6c38a7d63b13c228760
kyoungholee <dlrudgh1303@gmail.com>
2023-02-04 03:35:32 +0900

    [Feature]: 회원가입 유효성 검사 & 형식에 맞지 않을 경우 에러 메세지 구현& 회원가입 양식구현 & 자릿수 구현& 로컬스토리지 연동& 조건에 맞지 않을 경우 버튼 비활성화 및 활성화

--- client/pages/signup/signup.js
+++ client/pages/signup/signup.js
@@ -1,113 +1,227 @@
-// function checkSignup() {
 const id = document.getElementById('id');
 const password = document.getElementById('pw');
 const checkPw = document.getElementById('confirmpw');
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
+const confirmButton = document.querySelector('.confirm_check');
+const emailConfirmButton = document.querySelector('.email_confirm_check');
+const signupButton = document.querySelector('.signup_btn');
+const Congratulations = document.querySelector('.congratulations');
 
-//정규표현식 비밀번호 영문자 + 숫자 + 특수문자 조합(3~20자리 입력 )
-const pwdCheck = /^[a-z]+[a-z0-9]{5,17}$/g;
-const onlyEn = /^[a-z|A-Z]+$/;
-const numberAndEn = /^[a-z|A-Z|0-9|]+$/;
+//정규표현식 비밀번호 영문자 + 숫자 (5~17자리 입력 )
+const pwdCheck = /^[a-zA-Z0-9]{5,17}$/g;
+//오직 영어만!!  = true
+const onlyEn = /^[a-zA-Z]*$/;
+//영어랑 숫자
+const numberAndEn = /^[a-zA-Z0-9]*$/;
+//숫자만
+const phonenumber = /^[0-9]+$/;
+//한글
+const korean = /^[ㄱ-ㅎ|가-힣]+$/;
+//숫자 한글
+const koreaAndNumber = /^[ㄱ-ㅎ|가-힣0-9]+$/;
+//영어 한글 영어를 쓸수가 없는 에러
+const EnAndkorean = /^[a-z|A-Z|ㄱ-ㅎ|가-힣|]+$/;
 
 //키보드를 한번에 빨리치면 안먹는다.
 function idHandler(e) {
-  const target = e.target.value;
-  console.log(target);
-  const idContainer = document.getElementsByClassName('id_write');
+  const errorID = document.querySelector('.error_id');
 
-  if (target.length == 5) {
-    let newErrorTitle =
-      '<div class = "error_id">6자 이상 16자 이하의 영문 혹은 영문과 숫자를 조합하세요.</div>';
-    idContainer[0].insertAdjacentHTML('afterend', newErrorTitle);
-    document.getElementsByClassName('error_id')[0].style.cssText =
-      'font-size : 13px; color: rgb(240, 63, 64); margin-top: -4px; display: flex; margin-left: 149px; padding: 0 0 10px 0';
-  } //영어 소문자 이거나 숫자 + 소문자
-  else if (
-    (target.length == 6 && numberAndEn.test(target) == true) ||
-    (target.length == 6 && onlyEn.test(target) == true)
+  //id.value = target;
+  id.setAttribute('value', id.value);
+  console.log(id.value);
+  //console.log(numberAndEn.test(id.value));
+
+  if (
+    (id.value.length > 0 && id.value.length < 6) ||
+    phonenumber.test(id.value) ||
+    korean.test(id.value) ||
+    koreaAndNumber.test(id.value) ||
+    EnAndkorean.test(id.value)
   ) {
-    const removeErrorMg = document.querySelector('.error_id');
-    removeErrorMg.remove();
+    errorID.style.display = 'inline';
+  } else if (id.value.length == 0) {
+    errorID.style.display = 'inline';
+  } else if (numberAndEn.test(id.value) || onlyEn.test(id.value)) {
+    errorID.style.display = 'none';
+  } else {
+    errorID.style.display = 'none';
   }
 }
+
 id.addEventListener('keyup', idHandler);
 
-// function pwHandler(e) {
-//   const pwContainer = document.getElementsByClassName('password_write');
+function checkHandler(e) {
+  const getterId = localStorage.getItem('id');
 
-//   if (e.target.value === 'a') {
-//     let newErrorTitle =
-//       '<div class = "error_pw">최소 10자 이상 입력해주세요.</div>';
-//     pwContainer[0].insertAdjacentHTML('afterend', newErrorTitle);
-//     document.getElementsByClassName('error_pw')[0].style.cssText =
-//       'font-size : 13px; color: rgb(240, 63, 64); margin-top: -4px; display: flex; margin-left: 149px; padding: 0 0 10px 0';
-//   }
-// }
-// password.addEventListener('input', pwHandler);
+  //console.log(id.value);
+  if (id.value === getterId) {
+    alert('이미 추가되어있는 아이디입니다.');
+    id.value = null;
+  } else if (id.value == '') {
+    alert('6자 이상 16자 이하 영문 혹은 영문과 숫자를 조합하세요');
+  } else if (
+    phonenumber.test(id.value) ||
+    korean.test(id.value) ||
+    (id.value.length > 0 && id.value.length < 6) ||
+    koreaAndNumber.test(id.value) ||
+    EnAndkorean.test(id.value)
+  ) {
+    alert('양식에 맞지 않아요!!!!');
+    id.value = null;
+  } else {
+    alert('사용가능한 ID입니다.');
+    confirmButton.style.color = 'rgb(221, 221, 221)';
+    confirmButton.style.border = ' 1px solid rgb(221, 221, 221)';
+    confirmButton.setAttribute('disabled', '');
+    id.setAttribute('disabled', '');
+  }
+}
 
-// function pwConfirmHandler(e) {
-//   const pwConfirmContainer =
-//     document.getElementsByClassName('password_confirm');
+confirmButton.addEventListener('click', checkHandler);
 
-//   if (e.target.value != 'a') {
-//     let newErrorTitle =
-//       '<div class = "error_confirm_pw">동일한 비밀번호를 입력해주세요.</div>';
-//     pwConfirmContainer[0].insertAdjacentHTML('afterend', newErrorTitle);
-//     document.getElementsByClassName('error_confirm_pw')[0].style.cssText =
-//       'font-size : 13px; color: rgb(240, 63, 64); margin-top: -4px; display: flex; margin-left: 149px; padding: 0 0 10px 0';
-//   }
-// }
-// checkPw.addEventListener('input', pwConfirmHandler);
+function pwHandler(e) {
+  const errorPw = document.querySelector('.error_pw');
+  const noneErrorPw = document.querySelector('.none_error_pw');
+  const target = e.target.value;
+  console.log(target.length);
 
-// function nameHandler(e) {
-//   const nameContainer = document.getElementsByClassName('name_container');
+  if (target.length > 0 && target.length < 10) {
+    noneErrorPw.style.display = 'none';
+    errorPw.style.display = 'inline';
+  } else if (target.length == 0) {
+    errorPw.style.display = 'none';
+    noneErrorPw.style.display = 'inline';
+  } else {
+    errorPw.style.display = 'none';
+  }
+}
+password.addEventListener('keyup', pwHandler);
 
-//   if (e.target.value == '') {
-//     let newErrorTitle = '<div class = "error_name">이름을 입력해 주세요.</div>';
-//     nameContainer[0].insertAdjacentHTML('afterend', newErrorTitle);
-//     document.getElementsByClassName('error_name')[0].style.cssText =
-//       'font-size : 13px; color: rgb(240, 63, 64); margin-top: -4px; display: flex; margin-left: 149px; padding: 0 0 10px 0';
-//   }
-// }
-// name.addEventListener('input', nameHandler);
+function pwConfirmHandler(e) {
+  const error_confirm_pw = document.querySelector('.error_confirm_pw');
+  const none_error_confirm_pw = document.querySelector(
+    '.none_error_confirm_pw'
+  );
+  const asd = e.target.value;
+  console.log(asd);
 
-// function emailHandler(e) {
-//   const emailContainer = document.getElementsByClassName('email_container');
+  if (password.value !== checkPw.value) {
+    none_error_confirm_pw.style.display = 'none';
+    error_confirm_pw.style.display = 'inline';
+  } else if (asd.length == 0) {
+    error_confirm_pw.style.display = 'none';
+    none_error_confirm_pw.style.display = 'inline';
+  } else {
+    error_confirm_pw.style.display = 'none';
+  }
+}
+checkPw.addEventListener('keyup', pwConfirmHandler);
 
-//   if (e.target.value === 'a') {
-//     let newErrorTitle =
-//       '<div class = "error_email">이메일 형식으로 입력해주세요.</div>';
-//     emailContainer[0].insertAdjacentHTML('afterend', newErrorTitle);
-//     document.getElementsByClassName('error_email')[0].style.cssText =
-//       'font-size : 13px; color: rgb(240, 63, 64); margin-top: -4px; display: flex; margin-left: 149px; padding: 0 0 10px 0';
-//   } else if (e.target.value === '') {
-//     let newErrorTitle =
-//       '<div class = "none_email_error">이메일을 입력해 주세요.</div>';
-//     emailContainer[0].insertAdjacentHTML('afterend', newErrorTitle);
-//     document.getElementsByClassName('none_email_error')[0].style.cssText =
-//       'font-size : 13px; color: rgb(240, 63, 64); margin-top: -4px; display: flex; margin-left: 149px; padding: 0 0 10px 0';
-//   }
-// }
-// email.addEventListener('input', emailHandler);
+function nameHandler(e) {
+  const ErrorName = document.querySelector('.error_name');
+  const target = e.target.value;
 
-// //숫자만 입력 숫자 외는 경고문 ,  다 지워도 경고문
-// function phoneHandler(e) {
-//   const phoneContainer = document.getElementsByClassName('phone_container');
+  if (target.length == 0) {
+    ErrorName.style.display = 'inline';
+  } else {
+    ErrorName.style.display = 'none';
+  }
+}
+name.addEventListener('keyup', nameHandler);
 
-//   if (e.target.value === 'a') {
-//     let newErrorTitle =
-//       '<div class = "phone_error">휴대폰 번호를 입력해 주세요.</div>';
-//     phoneContainer[0].insertAdjacentHTML('afterend', newErrorTitle);
-//     document.getElementsByClassName('phone_error')[0].style.cssText =
-//       'font-size : 13px; color: rgb(240, 63, 64); margin-top: -4px; display: flex; margin-left: 149px; padding: 0 0 10px 0';
-//   } else if (e.target.value === '') {
-//     let newErrorTitle =
-//       '<div class = "phone_none_error">휴대폰 번호를 입력해 주세요.</div>';
-//     phoneContainer[0].insertAdjacentHTML('afterend', newErrorTitle);
-//     document.getElementsByClassName('phone_none_error')[0].style.cssText =
-//       'font-size : 13px; color: rgb(240, 63, 64); margin-top: -4px; display: flex; margin-left: 149px; padding: 0 0 10px 0';
-//   }
-// }
-// phone.addEventListener('input', phoneHandler);
+function emailHandler(e) {
+  const ErrorEmail = document.querySelector('.error_email');
+  const NoneErrorEmail = document.querySelector('.error_none_email');
+
+  console.log(email.value);
+
+  if (email.value.includes('.c')) {
+    ErrorEmail.style.display = 'none';
+    NoneErrorEmail.style.display = 'none';
+  } else if (email.value.length == 0) {
+    ErrorEmail.style.display = 'none';
+    NoneErrorEmail.style.display = 'inline';
+  } else {
+    NoneErrorEmail.style.display = 'none';
+    ErrorEmail.style.display = 'inline';
+  }
+}
+email.addEventListener('keyup', emailHandler);
+
+function EmailCheckHandler(e) {
+  const getterEmail = localStorage.getItem('email');
+
+  //console.log(id.value);
+  if (email.value === getterEmail) {
+    alert('이미 추가되어있는 이메일입니다.');
+    email.value = null;
+  } else if (email.value.length == 0) {
+    alert('이메일을 적어주세요.');
+  } else if (email.value.includes('.com') == false) {
+    alert('이메일 형식으로 입력해주세요.');
+    email.value = null;
+  } else {
+    alert('사용가능한 이메일 입니다.');
+    emailConfirmButton.style.color = 'rgb(221, 221, 221)';
+    emailConfirmButton.style.border = ' 1px solid rgb(221, 221, 221)';
+    emailConfirmButton.setAttribute('disabled', '');
+    email.setAttribute('disabled', '');
+  }
+}
+
+emailConfirmButton.addEventListener('click', EmailCheckHandler);
+
+function phoneHandler(e) {
+  const errorPhone = document.querySelector('.error_phone');
+  const regexp = /[^0-9]/gi;
+  phone.value = phone.value.replace(regexp, '');
+
+  if (phone.value.length == 0) {
+    errorPhone.style.display = 'inline';
+  } else if (phonenumber.test(phone.value) == true) {
+    errorPhone.style.display = 'none';
+  }
+}
+phone.addEventListener('keyup', phoneHandler);
+
+//버튼 클릭 시 로컬 스토리지 저장
+function signupHandler(e) {
+  e.preventDefault();
+  const currentId = id.value;
+  const currentPassword = password.value;
+  const currentemail = email.value;
+  localStorage.setItem('id', currentId);
+  localStorage.setItem('password', currentPassword);
+  localStorage.setItem('email', currentemail);
+
+  window.location = '../../pages/login/login.html';
+}
+
+Congratulations.addEventListener('click', signupHandler);
+
+const signupContairer = document.querySelector('.signup_wrapper');
+function joinHandler() {
+  if (id.value && password.value && name.value && email.value && phone.value) {
+    Congratulations.style.color = 'var(--primary)';
+    Congratulations.style.cursor = 'pointer';
+    Congratulations.style.border = ' 1px solid var(--primary)';
+    Congratulations.removeAttribute('disabled', 'disabled');
+    Congratulations.setAttribute('abled', 'abled');
+  } else if (
+    id.value == '' ||
+    password.value == 0 ||
+    name.value == '' ||
+    email.value == '' ||
+    phone.value == ''
+  ) {
+    Congratulations.removeAttribute('style');
+    Congratulations.style.color = 'rgb(221, 221, 221)';
+    Congratulations.style.border = ' 1px solid rgb(221, 221, 221)';
+    Congratulations.removeAttribute('abled', 'abled');
+    Congratulations.setAttribute('disabled', 'disabled');
+  }
+}
+
+signupContairer.addEventListener('keyup', joinHandler);
