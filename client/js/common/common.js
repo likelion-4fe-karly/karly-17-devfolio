724cd7a37bdca2031ab277f4ac8e45fd7c398d88
SSY1203 <tlstjsdud566@gmail.com>
2023-02-03 19:51:55 +0900

    [Feature] : 로그인 정보 로컬 스토리지에서 확인 및 헤더에 오른쪽 상단에 username 띄우기

--- client/js/common/common.js
+++ client/js/common/common.js
@@ -1,9 +1,9 @@
 import { includeIframeToHTML } from './../../lib/include/includeIframeToHTML.js';
+import { getNode, insertFirst } from './../../lib/index.js';
 
 includeIframeToHTML()
   .then(() => {
     const body = document.querySelector('body');
-
     const bodyHandler = (e) => {
       const category = e.target.closest('button');
       const subCategory = document.querySelector('.category_menu');
@@ -16,51 +16,85 @@ includeIframeToHTML()
 
     body.addEventListener('click', bodyHandler);
 
-    // ! 뷰티 플랫폼 POP UP 영역
-    const popup = document.querySelector('.beauty_popup');
-    const neverWatch = document.querySelector('.beauty_popup_never_watch');
-    const close = document.querySelector('.beauty_popup_close');
+    const userName = localStorage.getItem('userName');
+    const signUpLink = document.querySelector('.sign_up_link');
+    const blank = document.querySelector('.blank');
+    const loginLink = document.querySelector('.login_link');
 
-    /* 스토리지 제어 함수 정의 */
-    var handleStorage = {
-      // 스토리지에 데이터 쓰기(이름, 만료일)
-      setStorage: function (name, exp) {
-        // 만료 시간 구하기(exp를 ms단위로 변경)
-        var date = new Date();
-        date = date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
-
-        // 로컬 스토리지에 저장하기
-        // (값을 따로 저장하지 않고 만료 시간을 저장)
-        localStorage.setItem(name, date);
-      },
-      // 스토리지 읽어오기
-      getStorage: function (name) {
-        var now = new Date();
-        now = now.setTime(now.getTime());
-        // 현재 시각과 스토리지에 저장된 시각을 각각 비교하여
-        // 시간이 남아 있으면 true, 아니면 false 리턴
-        return parseInt(localStorage.getItem(name)) > now;
-      },
-    };
-
-    if (handleStorage.getStorage('today')) {
-      popup.classList.add('off');
-    } else {
-      popup.classList.remove('off');
+    if (localStorage.getItem('isLogin') === 'true') {
+      signUpLink.style.display = 'none';
+      blank.style.display = 'none';
+      loginLink.style.display = 'none';
+      insertFirst(
+        '.header_login ul',
+        /* html */ `
+              <li class="userName"><button>${userName}</button>님</li>  
+              <li class="newbar" aria-hidden="true"></li>      
+          `
+      );
     }
 
-    const noTodayHandler = () => {
-      handleStorage.setStorage('today', 1);
+    const logOut = document.querySelector('.userName button');
 
-      popup.classList.add('off');
+    const logOutHandler = () => {
+      localStorage.setItem('isLogin', false);
+      const user = getNode('.userName');
+      const bar = getNode('.newbar');
+      user.remove();
+      bar.remove();
+
+      signUpLink.style.display = 'inline-block';
+      blank.style.display = 'inline-block';
+      loginLink.style.display = 'inline-block';
     };
 
-    const closeHandler = () => {
-      popup.classList.add('off');
-    };
+    logOut.addEventListener('click', logOutHandler);
 
-    neverWatch.addEventListener('click', noTodayHandler);
-    close.addEventListener('click', closeHandler);
+    // // ! 뷰티 플랫폼 POP UP 영역
+    // const popup = document.querySelector('.beauty_popup');
+    // const neverWatch = document.querySelector('.beauty_popup_never_watch');
+    // const close = document.querySelector('.beauty_popup_close');
+
+    // /* 스토리지 제어 함수 정의 */
+    // var handleStorage = {
+    //   // 스토리지에 데이터 쓰기(이름, 만료일)
+    //   setStorage: function (name, exp) {
+    //     // 만료 시간 구하기(exp를 ms단위로 변경)
+    //     var date = new Date();
+    //     date = date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+
+    //     // 로컬 스토리지에 저장하기
+    //     // (값을 따로 저장하지 않고 만료 시간을 저장)
+    //     localStorage.setItem(name, date);
+    //   },
+    //   // 스토리지 읽어오기
+    //   getStorage: function (name) {
+    //     var now = new Date();
+    //     now = now.setTime(now.getTime());
+    //     // 현재 시각과 스토리지에 저장된 시각을 각각 비교하여
+    //     // 시간이 남아 있으면 true, 아니면 false 리턴
+    //     return parseInt(localStorage.getItem(name)) > now;
+    //   },
+    // };
+
+    // if (handleStorage.getStorage('today')) {
+    //   popup.classList.add('off');
+    // } else {
+    //   popup.classList.remove('off');
+    // }
+
+    // const noTodayHandler = () => {
+    //   handleStorage.setStorage('today', 1);
+
+    //   popup.classList.add('off');
+    // };
+
+    // const closeHandler = () => {
+    //   popup.classList.add('off');
+    // };
+
+    // neverWatch.addEventListener('click', noTodayHandler);
+    // close.addEventListener('click', closeHandler);
 
     // ! 장바구니 POP UP 영역
     const cartPopUpContainer = document.querySelector('.cart_popup_container');
