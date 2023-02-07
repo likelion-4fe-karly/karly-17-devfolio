cc3c40345c5478adfcf909c022a1f84b5747822e
kelly121212 <cbby1212@gmail.com>
2023-02-07 13:16:14 +0900

    [Modify] : 수정

--- client/pages/index/index2.js
+++ client/pages/index/index2.js
@@ -1,37 +1,3 @@
-import { insertLast } from './../../lib/dom/insert.js';
+// import { insertLast } from './../../lib/dom/insert.js';
 
-// TODO 비동기 통신 뿌리기
-
-fetch('http://localhost:3000/products')
-  .then((res) => res.json())
-  .then((json) => {
-    console.log(json);
-    let html = '';
-
-    json.forEach((product) => {
-      html += /*html*/ `
-    <li>
-    <a
-      class="index_recommend_product_swiper_item"
-      href="/pages/product_detail/product_detail.html"
-    >
-      <img
-        src="/assets/${product.image.view}"
-        alt="${product.image.alt}"
-      />
-      <p class="recommend_swiper_item_title">
-        ${product.name}
-      </p>
-      <p class="recommend_swiper_item_price">${product.price}원</p>
-    </a>
-    <div class="recommend_swiper_item_cart" tabindex="0">
-      <img
-        src="/assets/index/icon_cart.png"
-        alt="장바구니 버튼"
-      />
-    </div>
-  </li>`;
-    });
-
-    insertLast('.swiper-slide ul', html);
-  });
+//빈 배열을 하나 만들고 클릭한 대상(이미지)의 경로(주소)가 빈 배열에 담기도록  코드를 작성해보세요
