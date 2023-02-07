f0b76fa9360244583de2154e0b09d13f32b14a22
hamjongseok <irang4605@naver.com>
2023-02-07 09:41:46 +0900

    [Feature]: main swiper Banner기능 구현

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,33 +1,48 @@
 // import { getNode } from '../../lib/index.js';
 
+window.onload = function () {
+  fetch('http://localhost:3000/images')
+    .then((res) => res.json())
+    .then((images) => {
+      const swiperWrapper = document.querySelector('.swiper-wrapper');
+      images.forEach((image) => {
+        const slide = document.createElement('div');
+        slide.classList.add('swiper-slide');
+        const img = document.createElement('img');
+        img.src = image.src;
+        img.alt = image.id;
+        slide.appendChild(img);
+        swiperWrapper.appendChild(slide);
+      });
+    });
+  const swiper1 = new Swiper('.index_visual_swiper', {
+    autoplay: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    loop: true,
+    speed: 1000,
+    parallax: true,
+    a11y: {
+      prevSlideMessage: '이전 슬라이드',
+      nextSlideMessage: '다음 슬라이드',
+      slideLabelMessage:
+        '총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.',
+    },
+    navigation: {
+      prevEl: '.index_visual .swiper-button-prev',
+      nextEl: '.index_visual .swiper-button-next',
+    },
+  });
+};
+
 /* global Swiper */
 let swiperPrev = document.querySelector(
   '.index_recommend_product .swiper-button-prev'
 );
 
-console.log(swiperPrev);
-
 /* 메인 배너 */
-const swiper1 = new Swiper('.index_visual_swiper', {
-  autoplay: true,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
-  },
-  loop: true,
-  speed: 1000,
-  parallax: true,
-  a11y: {
-    prevSlideMessage: '이전 슬라이드',
-    nextSlideMessage: '다음 슬라이드',
-    slideLabelMessage:
-      '총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.',
-  },
-  navigation: {
-    prevEl: '.index_visual_swiper .swiper-button-prev',
-    nextEl: '.index_visual_swiper .swiper-button-next',
-  },
-});
 
 /* 이 상품 어때? */
 const swiper2 = new Swiper('.first .product', {
@@ -50,7 +65,6 @@ const swiper2 = new Swiper('.first .product', {
     activeIndexChange: function () {
       if (this.realIndex === 0) {
         swiperPrev.classList.add('swiper-button-disabled');
-        // startNum = false;
       } else {
         swiperPrev.classList.remove('swiper-button-disabled');
       }
