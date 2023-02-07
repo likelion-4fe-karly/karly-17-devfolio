571e11fc5632775c7ee5243f0e6a00ab20c403a2
kyoungholee <dlrudgh1303@gmail.com>
2023-02-07 10:11:37 +0900

    [Feature]: 로그인 시 메인페이지 사용자명으로 변경 && 로그아웃 기능 구현

--- client/js/common/common.js
+++ client/js/common/common.js
@@ -1,6 +1,6 @@
 import { includeIframeToHTML } from './../../lib/include/includeIframeToHTML.js';
 import { saveStorage } from './../../lib/utils/storage.js';
-
+import { getNode, insertFirst } from './../../lib/index.js';
 includeIframeToHTML()
   .then(() => {
     const body = document.querySelector('body');
@@ -87,6 +87,50 @@ includeIframeToHTML()
 
     cartButton.addEventListener('click', cartHandler);
     cartPopUpContainer.addEventListener('click', cartContainerHandler);
+
+    const userName = localStorage.getItem('name');
+    const signUpLink = document.querySelector('.sign_up_link');
+    const blank = document.querySelector('.blank');
+    const loginLink = document.querySelector('.login_link');
+    const centerLink = document.querySelector('.center_link');
+
+    if (localStorage.getItem('uid')) {
+      signUpLink.style.display = 'none';
+      blank.style.display = 'none';
+      loginLink.style.display = 'none';
+      centerLink.style.display = 'none';
+      insertFirst(
+        '.header_login ul',
+        /* html */ `
+              <li class="userName"><button>${userName}</button>님</li>  
+              <li class="newbar" aria-hidden="true"></li>
+              <li class="logout "><button>로그아웃</button></li>        
+          `
+      );
+    }
+
+    // 로그아웃 이벤트 핸들러
+    const logoutPage = document.querySelector('.logout');
+
+    function logoutPageHandler(e) {
+      console.log('로그아웃 됩니다.');
+      localStorage.removeItem('uid');
+      localStorage.removeItem('name');
+      const user = getNode('.userName');
+      const bar = getNode('.newbar');
+      const logout = getNode('.logout');
+      user.remove();
+      bar.remove();
+      logout.remove();
+
+      signUpLink.style.display = 'inline-block';
+      blank.style.display = 'inline-block';
+      loginLink.style.display = 'inline-block';
+      centerLink.style.display = 'inline-block';
+      window.location.reload();
+    }
+
+    logoutPage.addEventListener('click', logoutPageHandler);
   })
   .catch(() => {
     console.error('실패');
--- client/pages/login/login.js
+++ client/pages/login/login.js
@@ -2,7 +2,7 @@ const idInputTag = document.querySelector('.id_form_tag');
 const passwordInputTag = document.querySelector('.password_form_tag');
 const loginButton = document.querySelector('.login_button');
 
-function LoginIdHandler(e) {
+function LoginIdHandler() {
   idInputTag.setAttribute('value', idInputTag.value);
 }
 
@@ -14,30 +14,38 @@ function LoginPasswordHandler() {
 passwordInputTag.addEventListener('keyup', LoginPasswordHandler);
 
 function LoginButtonHandler(e) {
+  e.preventDefault();
   fetch('http://localhost:3000/user')
     //이부분이 promise 형태다 그럼 then이나 async await로 처리
 
     .then((response) => response.json())
     .then((data) => {
-      for (let i = 0; i < data.length; i++) {
-        console.log(passwordInputTag.value);
+      const filterData = data.filter(
+        //객체 1개씩
+        (obj) =>
+          obj.id === idInputTag.value && obj.password === passwordInputTag.value
+      );
+      console.log(filterData);
 
-        if (
-          passwordInputTag.value == Object.values(data[i])[1] &&
-          idInputTag.value == Object.values(data[i])[0]
-        ) {
-          console.log(Object.values(data[i])[3]);
-          localStorage.setItem('uid', Object.values(data[i])[3]);
-          alert('합격입니다.');
-          return;
-        } else {
-          continue;
-        }
+      if (filterData.length === 0) {
+        alert('일치하지 않습니다.');
+        idInputTag.value = null;
+        passwordInputTag.value = null;
+        return;
       }
-      idInputTag.value = null;
-      passwordInputTag.value = null;
-      return;
+
+      successLogin(filterData);
     });
 }
+
+const successLogin = (filterData) => {
+  console.log(filterData);
+  console.log(filterData[0].uid);
+  localStorage.setItem('uid', JSON.stringify(filterData[0].uid));
+  localStorage.setItem('name', filterData[0].name);
+  alert('환영합니다.');
+
+  window.location = '../../index.html';
+};
 
 loginButton.addEventListener('click', LoginButtonHandler);
